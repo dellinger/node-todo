c15b2e553db00141cc118fec2a83b1e5a616b8f7
David Ellinger <david.ellinger@me.com>
2014-12-11 22:57:19 -0800

    Fixed minor issue with new items not showing up

--- static.js
+++ static.js
@@ -25,10 +25,10 @@ function show(res) {
     var html = '<html><head><title>Todo List</title></head><body>' +
         '<h1>Todo List</h1>' +
         '<ul>' +
-         + items.map(function(item){
+        items.map(function(item){
              return '<li>' + item + '</li>'
-         }).join('')
-        + '</ul>' +
+         }).join('') +
+        '</ul>' +
         '<form method="post" action="/">' +
             '<p><input type="text" name="item" /></p>' +
         '<p><input type="submit" value="Add Item" /></p>' +
